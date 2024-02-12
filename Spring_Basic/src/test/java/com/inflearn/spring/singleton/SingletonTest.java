e631ab82e38a531d01231c07bf168d009d013fe5
happine2s <happine2s@sookmyung.ac.kr>
2024-02-12 13:45:44 +0900

    [싱글톤 컨테이너] 싱글톤 컨테이너

--- Spring_Basic/src/test/java/com/inflearn/spring/singleton/SingletonTest.java
+++ Spring_Basic/src/test/java/com/inflearn/spring/singleton/SingletonTest.java
@@ -6,6 +6,7 @@ import com.inflearn.spring.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
     @Test
@@ -37,5 +38,23 @@ public class SingletonTest {
 
         // 인스턴트 자체가 같은지를 비교
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1= ac.getBean("memberService", MemberService.class);
+        //2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2= ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1="+memberService1);
+        System.out.println("memberService2="+memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }

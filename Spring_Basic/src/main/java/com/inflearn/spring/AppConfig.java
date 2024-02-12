897f095a253eba8aa431e8593b1cb4206b7a0d17
happine2s <happine2s@sookmyung.ac.kr>
2024-02-12 10:09:16 +0900

    [객체 지향 원리 적용] AppConfig 리팩터링

--- Spring_Basic/src/main/java/com/inflearn/spring/AppConfig.java
+++ Spring_Basic/src/main/java/com/inflearn/spring/AppConfig.java
@@ -1,6 +1,8 @@
 package com.inflearn.spring;
 
+import com.inflearn.spring.discount.DiscountPolicy;
 import com.inflearn.spring.discount.FixDiscountPolicy;
+import com.inflearn.spring.member.MemberRepository;
 import com.inflearn.spring.member.MemberService;
 import com.inflearn.spring.member.MemberServiceImpl;
 import com.inflearn.spring.member.MemoryMemberRepository;
@@ -9,10 +11,17 @@ import com.inflearn.spring.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
